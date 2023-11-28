bd454982ac2b0c32a14b11845a224f16f89038dc
AnnaVolkodav <annvolkodav@gmail.com>
2023-11-29 00:11:12 +0200

    Home Tasks 1-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,14 @@
-function checkForSpam(message) { 
-    if(message.toLowerCase().includes('sale')) {
-        return true;
-    } else if(message.toLowerCase().includes('spam')) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+    const moreNumbers = [];
+    for(let appropriate of numbers) {
+        if(appropriate > value) {
+            moreNumbers.push(appropriate);
+        }
     }
+    return moreNumbers;
 }
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
